98c1e314157d6aeb8cdee486c66294fdd4e15d02
Dayane Ramos <dayanecode@google.com>
2022-07-01 20:05:27 -0300

    conecta com o banco de dados

---
+++ database.js
@@ -0,0 +1,13 @@
+const knex = require("knex")
+
+module.exports = knex({
+    client: 'mysql2',
+    connection: {
+        host : process.env.DATABASE_HOST,
+        user : process.env.DATABASE_USER,
+        password : process.env.DATABASE_PASS,
+        database : process.env.DATABASE_NAME
+    }
+})
+
+//conecta com o banco de dados
